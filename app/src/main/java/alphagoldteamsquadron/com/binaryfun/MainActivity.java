02049fc7f230b333410c72a6f06dac4284456895
zkello <zwk@case.edu>
2015-03-20 18:19:23 -0400

    Initial Commit

---
+++ app/src/main/java/alphagoldteamsquadron/com/binaryfun/MainActivity.java
@@ -0,0 +1,53 @@
+package alphagoldteamsquadron.com.binaryfun;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+    //Convention for keys is to put company name as prefix
+    public final static String EXTRA_MESSAGE = "com.alphagoldteamsquadron.binaryfun.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Ran when button is clicked
+    public void startGame(View view) {
+        Intent intent = new Intent(this, BinaryToDecimal.class);
+        String message = "Bananas";
+        //This is how we pass data to new activities
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //Starts binaryToDecimal activity
+        startActivity(intent);
+    }
+}
