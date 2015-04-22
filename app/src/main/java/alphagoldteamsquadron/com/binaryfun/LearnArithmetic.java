52388a64037c90483f2f7462084e0f8ced6599d3
Drew Mitchell <hi@callmedrew.com>
2015-04-22 19:50:29 -0400

    Submission 1 of learning content
    I wrote the first draft of the “Learn Binary” page and part of the
    “Learn Representations” (integer and floating point) pages. Pushing so
    that others can work on the other two pages if they please.

--- app/src/main/java/alphagoldteamsquadron/com/binaryfun/LearnArithmetic.java
+++ app/src/main/java/alphagoldteamsquadron/com/binaryfun/LearnArithmetic.java
@@ -2,6 +2,7 @@ package alphagoldteamsquadron.com.binaryfun;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 
 /**
@@ -9,12 +10,17 @@ import android.os.Bundle;
  */
 public class LearnArithmetic extends Activity {
 
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_learn_arithmetic);
+
+
+        webView = (WebView) findViewById(R.id.activity_main_arithmetic_webview);
+        webView.loadUrl("file:///android_asset/learn_arithmetic.html");
 
     }
 }
